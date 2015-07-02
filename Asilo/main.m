9ac5533fd849127a08d6622059d6e991f1f1480c
Octavio Cortes <ocortes@nearsoft.com>
2015-07-02 12:53:25 -0700

    initial code

---
+++ Asilo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Asilo
+//
+//  Created by Octavio Cortes on 7/2/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
