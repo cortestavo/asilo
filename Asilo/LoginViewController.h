e905a5c143af346a8b3d5cf5c080d0027f9936d5
Javo Rosas <javier.rosasb@gmail.com>
2015-07-07 12:46:33 -0700

    create Login first scene and controller

---
+++ Asilo/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Asilo
+//
+//  Created by Javier Rosas on 7/7/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
