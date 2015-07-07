31bb0bef74e0d78ae1b9d2b631dddd2d1cd0e9de
Octavio Cortes <ocortes@nearsoft.com>
2015-07-07 12:13:21 -0700

    Enabling parse in AppDelegate

--- Asilo/AppDelegate.m
+++ Asilo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [Parse setApplicationId:@"47azMepKryfWJSRJQYsGW93O2r3ltrn6QYPscHR8"
+                  clientKey:@"tp8yTvbgtJwbeN2FLRyqYxQQk6ZUZIGL8UNc59af"];
     return YES;
 }
 
