792cef74f08b594f5b89cf2fe68975d193561317
Javo Rosas <javier.rosasb@gmail.com>
2015-07-08 18:27:12 -0700

    add custom navigation controller

---
+++ Asilo/SearchNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  SearchNavigationController.m
+//  Asilo
+//
+//  Created by Javier Rosas on 7/8/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import "SearchNavigationController.h"
+
+@interface SearchNavigationController ()
+
+@end
+
+@implementation SearchNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
