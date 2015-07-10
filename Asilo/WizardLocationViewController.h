fa779bc3b3b42d3e1434be0e34260266a2b10b90
Javo Rosas <javier.rosasb@gmail.com>
2015-07-10 12:10:34 -0700

    add first scene of the Publish Wizard

---
+++ Asilo/WizardLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  WizardLocationViewController.h
+//  Asilo
+//
+//  Created by Javier Rosas on 7/10/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WizardLocationViewController : UIViewController
+
+@end
