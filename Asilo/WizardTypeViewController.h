30c44e237b047b6ec2307713399856bd0681d35e
Javo Rosas <javier.rosasb@gmail.com>
2015-07-13 12:16:01 -0700

    add selectType scene and VC

---
+++ Asilo/WizardTypeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WizardTypeViewController.h
+//  Asilo
+//
+//  Created by Javier Rosas on 7/10/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WizardTypeViewController : UIViewController
+
+@end
