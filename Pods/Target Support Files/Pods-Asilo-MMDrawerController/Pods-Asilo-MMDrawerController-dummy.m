37b5bbcec4ed9edec03d6802a3dac80831f035f2
Javo Rosas <javier.rosasb@gmail.com>
2015-07-16 11:12:05 -0700

    hotfix: remove parse pod and add it locally

--- Pods/Target Support Files/Pods-Asilo-MMDrawerController/Pods-Asilo-MMDrawerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Asilo_MMDrawerController : NSObject
-@end
-@implementation PodsDummy_Pods_Asilo_MMDrawerController
-@end
--- Pods/Target Support Files/Pods-Asilo-Parse/Pods-Asilo-Parse-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Asilo_Parse : NSObject
-@end
-@implementation PodsDummy_Pods_Asilo_Parse
-@end
