90475e4f9f3711a5e76aff04945412b82a243b8d
Javo Rosas <javier.rosasb@gmail.com>
2015-07-16 16:53:55 -0700

    add List cell

---
+++ Asilo/ResultListTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ResultListTableViewCell.h
+//  Asilo
+//
+//  Created by Javier Rosas on 7/15/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ASHome.h"
+
+@interface ResultListTableViewCell : UITableViewCell
+
+- (void)setupWithHome:(ASHome *)home;
+
+@end
