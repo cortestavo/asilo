26f83548b7ac087a28a063c906a882c26f360fbc
Octavio Cortes <ocortes@nearsoft.com>
2015-07-17 11:42:11 -0700

    Showing map with centered in Hermosillo and find homes in that area

--- Asilo/ResultMapViewController.m
+++ Asilo/ResultMapViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ResultMapViewController.h"
+#import "ASFilter.h"
+#import "SearchNavigationController.h"
+#import "ASHomeRepository.h"
+#import "ASHome.h"
 
 @interface ResultMapViewController ()
+
+@property (assign, nonatomic) ASFilterType searchType;
 
 @end
 
@@ -16,12 +22,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.searchType = ((SearchNavigationController *)self.parentViewController).searchType;
+    [self initMap];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/**
+    Creates a map with Hermosillo as a center
+**/
+- (void)initMap {
+    self.mapView.delegate = self;
+    CLLocationDistance distance = 35 * 1609;
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 29.0729670;
+    zoomLocation.longitude = -110.9559190;
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, distance, distance);
+    [self.mapView setRegion:viewRegion animated:YES];
+}
+
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
+    [self getHomes];
+}
+
+- (void) getHomes {
+    MKMapRect mRect = self.mapView.visibleMapRect;
+    CLLocationCoordinate2D ne = MKCoordinateForMapPoint(MKMapPointMake(MKMapRectGetMaxX(mRect), mRect.origin.y));
+    CLLocationCoordinate2D sw = MKCoordinateForMapPoint(MKMapPointMake(mRect.origin.x, MKMapRectGetMaxY(mRect)));
+    
+    [ASHomeRepository findByAreaWithNorthEast:ne southWest:sw searchType:self.searchType block:^void (NSArray *homes){
+        NSLog(@"%@", ((ASHome *)homes[0]).description);
+    }];
 }
 
 /*
