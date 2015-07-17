16b057f3c77742b7f259d2caa9e777ea33b864f8
Javo Rosas <javier.rosasb@gmail.com>
2015-07-16 19:17:10 -0700

    add empty scene and VC

---
+++ Asilo/HomeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeDetailViewController.h
+//  Asilo
+//
+//  Created by Javier Rosas on 7/16/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeDetailViewController : UIViewController
+
+@end
