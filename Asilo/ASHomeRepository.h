2e78807126eabb62c47709a93eb4d068b9e55d48
Octavio Cortes <ocortes@nearsoft.com>
2015-07-17 11:37:50 -0700

    Repository for Home management

---
+++ Asilo/ASHomeRepository.h
@@ -0,0 +1,17 @@
+//
+//  ASHomeRepository.h
+//  Asilo
+//
+//  Created by Octavio Cortes on 7/16/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import <MapKit/MapKit.h>
+#import "ASFilter.h"
+
+@interface ASHomeRepository : NSObject 
+
++ (void) findByAreaWithNorthEast:(CLLocationCoordinate2D)northEastPoint southWest:(CLLocationCoordinate2D)southWestPoint searchType:(ASFilterType)searchType block:(void (^)(NSArray *homes))block;
+
+@end
