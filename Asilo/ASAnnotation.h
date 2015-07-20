af0d25d0397f91fc3bd0f4650ef20df91a66d69c
Octavio Cortes <ocortes@Octavios-MacBook-Pro.local>
2015-07-20 11:08:01 -0700

    custom annotation class

---
+++ Asilo/ASAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  ASAnnotation.h
+//  Asilo
+//
+//  Created by Octavio Cortes on 7/20/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface ASAnnotation : MKPointAnnotation
+
+@end
---
+++ Asilo/ASAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  ASAnnotation.m
+//  Asilo
+//
+//  Created by Octavio Cortes on 7/20/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import "ASAnnotation.h"
+
+@implementation ASAnnotation
+
+@end
