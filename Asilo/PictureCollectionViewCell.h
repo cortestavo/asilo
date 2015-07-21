7ef451aac52fa4f036f59e65d3e44c1cfcdc8099
Javo Rosas <javier.rosasb@gmail.com>
2015-07-21 12:32:15 -0700

    add custom collectionview cell for pictures

---
+++ Asilo/PictureCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  PictureCollectionViewCell.h
+//  Asilo
+//
+//  Created by Javier Rosas on 7/21/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PictureCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
