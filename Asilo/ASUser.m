1c887d8fc08ae86e35650885707cdf5c55766d38
Octavio Cortes <ocortes@Octavios-MacBook-Pro.local>
2015-07-22 17:42:49 -0700

    methods to favorite and unfavorite a home

--- Asilo/ASUser.m
+++ Asilo/ASUser.m
@@ -8,6 +8,7 @@
 
 #import "ASUser.h"
 #import "ASHomeRepository.h"
+#import "ASHome.h"
 
 @implementation ASUser
 
@@ -24,6 +25,32 @@
 
 - (void)getMyPublicationsWithBlock:(void(^)(NSArray *, NSError *))block {
     [ASHomeRepository getPublishedHomesForUser:self block:block];
+}
+
+- (void) getFavoritesWithBlock:(void (^)(NSMutableArray *))block {
+    PFRelation *favoritesRelation = [self relationForKey:@"favorites"];
+    PFQuery *favoritesQuery = [favoritesRelation query];
+    [favoritesQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error){
+        if(objects == nil) {
+            objects = [NSArray array];
+        }
+    }];
+}
+
+- (void) favorite:(ASHome *)home block:(void (^)())block {
+    PFRelation *favoritesRelation = [self relationForKey:@"favorites"];
+    [favoritesRelation addObject:home];
+    [self saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error){
+        block();
+    }];
+}
+
+- (void) unfavorite:(ASHome *)home block:(void (^)())block {
+    PFRelation *favoritesRelation = [self relationForKey:@"favorites"];
+    [favoritesRelation removeObject:home];
+    [self saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error){
+        block();
+    }];
 }
 
 @end
