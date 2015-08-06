e7534b78f2f77ad04a8624a462bc990be430a21f
Octavio Cortes <ocortes@Octavios-MacBook-Pro.local>
2015-08-05 18:15:40 -0700

    adding new menu element

---
+++ Asilo/FavoritesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesTableViewController.h
+//  Asilo
+//
+//  Created by Octavio Cortes on 8/5/15.
+//  Copyright (c) 2015 Nearsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesTableViewController : UITableViewController
+
+@end
